711925af71144bf76b63acd53e0cd1f0866b90b6
Miguel Angel <vivanlasfallas@hotmail.com>
2021-07-01 15:52:48 +0200

    Update pages.module.ts

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
-import { AppRoutingModule } from '../../../../authApp/src/app/app-routing.module';
+import { AppRoutingModule } from '../app-routing.module';
+
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PagesComponent } from './pages.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { AuthModule } from '../auth/auth.module';
+
 
 
 
