01c96dfa7fc74d632ed11f7e1188d8a4d5b1e85c
Miguel Angel <vivanlasfallas@hotmail.com>
2021-07-01 15:46:34 +0200

    Añadidas rutas hijas y módulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { AppRoutingModule } from '../../../../authApp/src/app/app-routing.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { AuthModule } from '../auth/auth.module';
+
+
+
+@NgModule({
+  declarations: [
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    AppRoutingModule,
+    AuthModule
+  ],
+  exports:[
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+    DashboardComponent
+  ]
+})
+export class PagesModule { }
